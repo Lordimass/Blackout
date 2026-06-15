5231b68de1f66e8e6e6a73aa592d939d6934e9b7
Riprod <riprodgames@gmail.com>
2026-06-15 18:08:11 -0400

    fix: import paths

--- src/main/java/com/perl/blackout/BlackoutPlugin.java
+++ src/main/java/com/perl/blackout/BlackoutPlugin.java
@@ -1,16 +1,17 @@
-package com.riprod.patchly;
+package com.perl.blackout;
 
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import com.riprod.patchly.PatchManager;
 
-public final class PatchlyPlugin extends JavaPlugin {
+public final class BlackoutPlugin extends JavaPlugin {
     private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
     private final PatchManager patchManager;
 
-    public PatchlyPlugin(JavaPluginInit init) {
+    public BlackoutPlugin(JavaPluginInit init) {
         super(init);
-        LOGGER.atInfo().log("Patchly %s initializing...", PatchManager.PATCHER_VERSION);
+        LOGGER.atInfo().log("Blackout %s initializing...", PatchManager.PATCHER_VERSION);
         patchManager = new PatchManager(this);
     }
 
